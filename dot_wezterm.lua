203451729ee8660a7c983c7217418d6909f977c3
Avishay Matayev <me@avishay.dev>
2023-10-18 21:46:28 +0300

    wezterm: add basic config file

---
+++ dot_wezterm.lua
@@ -0,0 +1,20 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+-- This is where you actually apply your config choices
+config.font = wezterm.font 'FiraCodeNerdFont'
+config.enable_tab_bar = false
+config.window_decorations = "NONE"
+config.color_scheme = 'AdventureTime'
+
+-- and finally, return the configuration to wezterm
+return config
