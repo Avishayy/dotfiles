3bfb38f42da6f6cd8a7e209256258f2c727d8df3
Avishay Matayev <me@avishay.dev>
2023-10-27 16:37:27 +0300

    formatting (nvim): change null-ls to conform.nvim
    The former is deprecated

--- dot_config/nvim/lua/plugins/formatting.lua
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -1,60 +1,36 @@
-local async_formatting = function(bufnr)
-  bufnr = bufnr or vim.api.nvim_get_current_buf()
-
-  vim.lsp.buf_request(bufnr, "textDocument/formatting", vim.lsp.util.make_formatting_params {}, function(err, res, ctx)
-    if err then
-      local err_msg = type(err) == "string" and err or err.message
-      -- you can modify the log message / level (or ignore it completely)
-      vim.notify("formatting: " .. err_msg, vim.log.levels.WARN)
-      return
-    end
-
-    -- don't apply results if buffer is unloaded or has been modified
-    if not vim.api.nvim_buf_is_loaded(bufnr) or vim.api.nvim_buf_get_option(bufnr, "modified") then
-      return
-    end
-
-    if res then
-      local client = vim.lsp.get_client_by_id(ctx.client_id)
-      vim.lsp.util.apply_text_edits(res, bufnr, client and client.offset_encoding or "utf-16")
-      vim.api.nvim_buf_call(bufnr, function()
-        vim.cmd("silent noautocmd update")
-      end)
-    end
-  end)
-end
-
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim",
-    event = "VeryLazy",
-    dependencies = {
-      { "nvim-lua/plenary.nvim" },
-    },
-    config = function()
-      local null_ls = require("null-ls")
-      null_ls.setup {
-        sources = {
-          null_ls.builtins.formatting.prettierd,
-          null_ls.builtins.formatting.black,
-          null_ls.builtins.formatting.isort,
-          null_ls.builtins.formatting.rustfmt,
-          null_ls.builtins.formatting.stylua,
-        },
-        on_attach = function(client, bufnr)
-          if client.supports_method("textDocument/formatting") then
-            vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
-            vim.api.nvim_create_autocmd("BufWritePost", {
-              group = augroup,
-              buffer = bufnr,
-              callback = function()
-                async_formatting(bufnr)
-              end,
-            })
-          end
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    keys = {
+      {
+        "<leader>f",
+        function()
+          require("conform").format({ async = true, timeout_ms = 500, lsp_fallback = true })
         end,
-      }
+        mode = "",
+        desc = "Format buffer",
+      },
+    },
+    opts = {
+      formatters_by_ft = {
+        javascript = { 'prettierd' },
+        typescript = { 'prettierd' },
+        javascriptreact = { 'prettierd' },
+        typescriptreact = { 'prettierd' },
+        python = { 'black', 'isort' },
+        rust = { 'rustfmt' },
+        lua = { 'stylua' },
+      },
+      format_on_save = {
+        async = true,
+        timeout_ms = 500,
+        lsp_fallback = true,
+      },
+    },
+    init = function()
+      vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,
   },
 }
