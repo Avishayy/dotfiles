90d63d264c8dc81fe426a4d7b3652e8044d80c31
Avishay Matayev <me@avishay.dev>
2023-11-08 19:16:33 +0200

    lint: Add autocmds for running lint

--- dot_config/nvim/lua/plugins/lint.lua
+++ dot_config/nvim/lua/plugins/lint.lua
@@ -6,6 +6,16 @@ return {
       require("lint").linters_by_ft = {
         python = { "ruff" },
       }
+
+      -- Refresh lints when leaving insert mode / saving
+      vim.api.nvim_create_autocmd({ "BufReadPost", "BufWritePost", "InsertLeave" }, {
+        callback = function()
+          local lint_status, lint = pcall(require, "lint")
+          if lint_status then
+            lint.try_lint()
+          end
+        end,
+      })
     end,
 
     -- for ruff, conform.nvim already applies lint fixes
