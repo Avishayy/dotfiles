8bed0a755f7cc9b7914d8d7992343c491f13ce79
Avishay Matayev <me@avishay.dev>
2024-06-25 02:44:02 +0300

    nvim: dap: fix deprecation error

--- dot_config/nvim/lua/plugins/dap.lua
+++ dot_config/nvim/lua/plugins/dap.lua
@@ -250,7 +250,7 @@ return {
     -- setup dap autocomplition
     require("cmp").setup {
       enabled = function()
-        return vim.api.nvim_buf_get_option(0, "buftype") ~= "prompt" or require("cmp_dap").is_dap_buffer()
+        return vim.api.nvim_get_option_value("buftype", { buf = 0 }) ~= "prompt" or require("cmp_dap").is_dap_buffer()
       end,
     }
 
