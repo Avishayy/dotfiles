51435065b851770e6669bae38cc70c1d7a4d9e39
Avishay Matayev <me@avishay.dev>
2024-06-25 02:41:37 +0300

    nvim: formatting: change slow formatters to async

--- dot_config/nvim/lua/plugins/formatting.lua
+++ dot_config/nvim/lua/plugins/formatting.lua
@@ -13,21 +13,39 @@ return {
         desc = "Format buffer",
       },
     },
-    opts = {
-      formatters_by_ft = {
-        javascript = { "eslint_d", "prettierd" },
-        typescript = { "eslint_d", "prettierd" },
-        javascriptreact = { "eslint_d", "prettierd" },
-        typescriptreact = { "eslint_d", "prettierd" },
-        python = { { "ruff" }, { "black", "isort" } },
-        rust = { "rustfmt" },
-        lua = { "stylua" },
-      },
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
-    },
+    config = function()
+      local slow_format_filetypes = {}
+      require("conform").setup {
+        format_on_save = function(bufnr)
+          if slow_format_filetypes[vim.bo[bufnr].filetype] then
+            return
+          end
+          local function on_format(err)
+            if err and err:match("timeout$") then
+              slow_format_filetypes[vim.bo[bufnr].filetype] = true
+            end
+          end
+
+          return { timeout_ms = 200, lsp_fallback = true }, on_format
+        end,
+
+        format_after_save = function(bufnr)
+          if not slow_format_filetypes[vim.bo[bufnr].filetype] then
+            return
+          end
+          return { lsp_fallback = true }
+        end,
+        formatters_by_ft = {
+          javascript = { "eslint_d", "prettierd" },
+          typescript = { "eslint_d", "prettierd" },
+          javascriptreact = { "eslint_d", "prettierd" },
+          typescriptreact = { "eslint_d", "prettierd" },
+          python = { { "ruff" }, { "black", "isort" } },
+          rust = { "rustfmt" },
+          lua = { "stylua" },
+        },
+      }
+    end,
     init = function()
       vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,
