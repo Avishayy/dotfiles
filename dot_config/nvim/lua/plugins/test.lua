3ab5fcf1de85ebf80deb8a1deb685ad382fde21d
Avishay Matayev <avishay.matayev@finaloop.com>
2024-09-08 11:26:43 +0300

    nvim: test: add neotest-jest

--- dot_config/nvim/lua/plugins/test.lua
+++ dot_config/nvim/lua/plugins/test.lua
@@ -4,6 +4,7 @@ return {
     dependencies = {
       "nvim-neotest/nvim-nio",
       "nvim-neotest/neotest-python",
+      "nvim-neotest/neotest-jest",
       "nvim-lua/plenary.nvim",
       "antoinemadec/FixCursorHold.nvim",
       "nvim-treesitter/nvim-treesitter",
@@ -15,6 +16,14 @@ return {
         adapters = {
           require("neotest-python") {
             dap = { justMyCode = false },
+          },
+          require("neotest-jest") {
+            jestCommand = "npm test --",
+            jestConfigFile = "custom.jest.config.ts",
+            env = { CI = true },
+            cwd = function(path)
+              return vim.fn.getcwd()
+            end,
           },
         },
       }
