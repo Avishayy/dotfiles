e61ee9b39a1f4cca32fad333eab154c6c12908c4
Avishay Matayev <me@avishay.dev>
2025-11-01 16:51:06 +0200

    migrate lspconfig to 0.11 nvim and use copilot

--- dot_config/nvim/lua/plugins/lsp.lua
+++ dot_config/nvim/lua/plugins/lsp.lua
@@ -1,35 +1,41 @@
-local on_attach = function(client, bufnr)
-  -- Enable completion triggered by <c-x><c-o>
-  vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+-- Setup LspAttach autocmd to configure buffer-local keymaps and options
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function(args)
+    local bufnr = args.buf
+    local client = vim.lsp.get_client_by_id(args.data.client_id)
 
-  -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  local bufopts = { noremap = true, silent = true, buffer = bufnr }
-  local nsilent_bufopts = { noremap = true, buffer = bufnr }
+    -- Enable completion triggered by <c-x><c-o>
+    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-  vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-  vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-  vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-  vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-  vim.keymap.set("n", "gh", vim.lsp.buf.signature_help, bufopts)
-  vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
-  vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
-  vim.keymap.set("n", "<space>wl", function()
-    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-  end, bufopts)
-  vim.keymap.set("n", "<space>s", vim.lsp.buf.workspace_symbol, nsilent_bufopts)
-  vim.keymap.set("n", "<space>d", vim.lsp.buf.document_symbol, bufopts)
-  vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
-  -- Uses IncRename instead
-  -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-  vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-  -- code actions is setup in actions-preview below
+    -- Mappings.
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    local bufopts = { noremap = true, silent = true, buffer = bufnr }
+    local nsilent_bufopts = { noremap = true, buffer = bufnr }
 
-  if client.server_capabilities.semanticTokensProvider then
-    vim.lsp.semantic_tokens.start(bufnr, client.id)
-    vim.cmd([[ highlight link @interface @type ]])
-  end
-end
+    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+    vim.keymap.set("n", "gh", vim.lsp.buf.signature_help, bufopts)
+    vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wr", vim.lsp.buf.remove_workspace_folder, bufopts)
+    vim.keymap.set("n", "<space>wl", function()
+      print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+    end, bufopts)
+    vim.keymap.set("n", "<space>s", vim.lsp.buf.workspace_symbol, nsilent_bufopts)
+    vim.keymap.set("n", "<space>d", vim.lsp.buf.document_symbol, bufopts)
+    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+    -- Uses IncRename instead
+    -- vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
+    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+    -- code actions is setup in actions-preview below
+
+    if client.server_capabilities.semanticTokensProvider then
+      vim.lsp.semantic_tokens.start(bufnr, client.id)
+      vim.cmd([[ highlight link @interface @type ]])
+    end
+  end,
+})
 
 return {
   {
@@ -59,21 +65,25 @@ return {
       "hrsh7th/nvim-cmp",
       "folke/neodev.nvim",
       "b0o/SchemaStore.nvim",
-      "simrat39/rust-tools.nvim",
       "nvim-telescope/telescope.nvim",
       "aznhe21/actions-preview.nvim",
     },
     config = function()
-      local capabilities = require("cmp_nvim_lsp").default_capabilities()
-      capabilities.textDocument.completion.completionItem.snippetSupport = true
-
+      -- Setup neodev for Neovim Lua development
       require("neodev").setup {
         library = { plugins = { "nvim-dap-ui" }, types = true },
       }
 
-      require("lspconfig").lua_ls.setup {
+      -- Configure global capabilities from nvim-cmp
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
+
+      vim.lsp.config("*", {
         capabilities = capabilities,
-        on_attach = on_attach,
+      })
+
+      -- Lua Language Server
+      vim.lsp.config("lua_ls", {
         settings = {
           Lua = {
             workspace = {
@@ -88,22 +98,22 @@ return {
             },
           },
         },
-      }
+      })
+      vim.lsp.enable("lua_ls")
 
-      require("lspconfig").jsonls.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
+      -- JSON Language Server with SchemaStore
+      vim.lsp.config("jsonls", {
         settings = {
           json = {
             schemas = require("schemastore").json.schemas(),
             validate = { enable = true },
           },
         },
-      }
+      })
+      vim.lsp.enable("jsonls")
 
-      require("lspconfig").pyright.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
+      -- Pyright with virtual environment detection
+      vim.lsp.config("pyright", {
         before_init = function(_, config)
           local Path = require("plenary.path")
 
@@ -119,72 +129,50 @@ return {
             config.settings.python.pythonPath = tostring(venv:joinpath("bin", "python"))
           end
         end,
-      }
+      })
+      vim.lsp.enable("pyright")
 
-      require("lspconfig").denols.setup {
-        on_attach = on_attach,
-        root_dir = require("lspconfig").util.root_pattern("deno.json", "deno.jsonc"),
-        single_file_support = false,
-      }
+      -- Deno Language Server (only start if deno.json/deno.jsonc exists)
+      vim.lsp.config("denols", {
+        root_markers = { "deno.json", "deno.jsonc" },
+        workspace_required = true,
+      })
+      vim.lsp.enable("denols")
 
-      require("lspconfig").clangd.setup {
-        capabilities = capabilities,
-        on_attach = on_attach,
-      }
+      -- C/C++ Language Server
+      vim.lsp.enable("clangd")
 
-      require("rust-tools").setup {
-        server = {
-          on_attach = on_attach,
-        },
-      }
+      -- Rust Analyzer (handled by rustaceanvim plugin)
+      -- Note: rustaceanvim is configured separately and doesn't need setup here
 
-      require("lspconfig").kotlin_language_server.setup {
-        server = {
-          capabilities = capabilities,
-          on_attach = on_attach,
-        },
-      }
+      -- Kotlin Language Server
+      vim.lsp.enable("kotlin_language_server")
 
-      require("lspconfig").html.setup {
-        server = {
-          capabilities = capabilities,
-          on_attach = on_attach,
-        },
-      }
+      -- HTML Language Server
+      vim.lsp.enable("html")
 
-      require("lspconfig").tailwindcss.setup {
-        server = {
-          capabilities = capabilities,
-          on_attach = on_attach,
-        },
-      }
+      -- Tailwind CSS Language Server
+      vim.lsp.enable("tailwindcss")
 
-      require("lspconfig").cssls.setup {
-        server = {
-          capabilities = capabilities,
-          on_attach = on_attach,
-        },
-      }
+      -- CSS Language Server
+      vim.lsp.enable("cssls")
 
-      require("lspconfig").cssmodules_ls.setup {
-        server = {
-          capabilities = capabilities,
-          on_attach = on_attach,
-        },
-      }
+      -- CSS Modules Language Server
+      vim.lsp.enable("cssmodules_ls")
 
-      require("lspconfig").graphql.setup {
-        server = {
-          capabilities = capabilities,
-          on_attach = on_attach,
-        },
-        root_dir = require("lspconfig").util.root_pattern(
+      -- GraphQL Language Server
+      vim.lsp.config("graphql", {
+        root_markers = {
           ".git",
           ".graphqlrc*",
           ".graphql.config.*",
-          "graphql.config.*"
-        ),
-      }
+          "graphql.config.*",
+        },
+      })
+      vim.lsp.enable("graphql")
+
+      -- Copilot Language Server
+      vim.lsp.enable("copilot")
     end,
   },
   {
@@ -210,6 +198,7 @@ return {
         automatic_enable = false,
         ensure_installed = {
           "biome",
+          "copilot",
           "cssls",
           "cssmodules_ls",
           "denols",
@@ -254,12 +243,13 @@ return {
       require("typescript-tools").setup {
         single_file_support = false,
         root_dir = function(fname)
-          if require("lspconfig.util").root_pattern("deno.json", "deno.jsonc")(fname) then
+          -- Avoid starting if deno.json/deno.jsonc exists (let denols handle it)
+          local util = require("lspconfig.util")
+          if util.root_pattern("deno.json", "deno.jsonc")(fname) then
             return nil
           end
-          return require("lspconfig.util").root_pattern("package.json", "tsconfig.json")(fname)
+          return util.root_pattern("package.json", "tsconfig.json")(fname)
         end,
-        on_attach = on_attach,
         settings = {
           tsserver_max_memory = 16384,
           tsserver_file_preferences = {
@@ -270,5 +260,11 @@ return {
         },
       }
     end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    version = "^5",
+    lazy = false,
+    ft = { "rust" },
   },
 }
